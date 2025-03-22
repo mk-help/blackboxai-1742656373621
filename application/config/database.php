7a1164d780c2de2e15baa67c9bbbe9bcfee4fad7
mk-help <61706211+mk-help@users.noreply.github.com>
2025-03-22 12:12:58 -0300

    Add application/config/database.php 1742656378006

---
+++ application/config/database.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn'          => '',
+    'hostname'     => 'localhost',
+    'username'     => 'root',  // Change in production
+    'password'     => '',      // Change in production
+    'database'     => 'membrosflix',
+    'dbdriver'     => 'mysqli',
+    'dbprefix'     => '',
+    'pconnect'     => FALSE,
+    'db_debug'     => (ENVIRONMENT !== 'production'),
+    'cache_on'     => FALSE,
+    'cachedir'     => '',
+    'char_set'     => 'utf8mb4',
+    'dbcollat'     => 'utf8mb4_unicode_ci',
+    'swap_pre'     => '',
+    'encrypt'      => FALSE,
+    'compress'     => FALSE,
+    'stricton'     => TRUE,
+    'failover'     => array(),
+    'save_queries' => TRUE
+);
+
+/* End of file database.php */
+/* Location: ./application/config/database.php */
